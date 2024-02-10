261ada00f42efeb6bad6252561e78ef883a8368f
Vanisha Bagga <vanishabagga@Vanishas-MacBook-Pro.local>
2024-02-10 11:47:36 -0500

    TF calculation in indexing

--- Assignment1/index.py
+++ Assignment1/index.py
@@ -12,16 +12,25 @@ import nltk
 from nltk.tokenize import word_tokenize
 from nltk.stem import PorterStemmer
 
+# counts how many times word appears in doc (TF):
+def countWordsInLine(token, processedTokens):
+  count = 0
+  for x in processedTokens:
+    if x == token:
+      count = count + 1
+  return float(count)
+
 def index(d):
     token = dict()
-    count = 0
-    for key in d:
+    for key in d: # each doc in dictionary
+        visited = set() # stores tokens that have been already visitied in the doc
         for word in d[key]:
-            if word not in token:
-                token[word] = {key}
-            else:
-                token[word].add(key)
-                count += 1
+            if word not in visited: 
+                if word not in token: # if word is not already in token dic
+                    token[word] = [(key, countWordsInLine(word, d[key]))] # tuple with doc ID and TF
+                else:
+                    token[word].append((key, countWordsInLine(word, d[key])))
+                visited.add(word)
     return token
 
 
